347e8be11193edb46fce6a9c27382f6946e34f31
Bonekazz <juracirdantas@gmail.com>
2024-03-22 18:38:33 -0300

    feat > add a view to the UserPomodorController

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { UserPomodorController } from "./controllers/userPomodor.controller";
 import { UserPomodorService } from "./services/userPomodor.service";
+import { UserPomodorView } from "./views/userPomodor.view";
 
-const userPomodorController = new UserPomodorController(new UserPomodorService);
+const userPomodorController = new UserPomodorController(new UserPomodorService, new UserPomodorView);
