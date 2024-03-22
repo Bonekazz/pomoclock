3969c49ec4b79a1032b4a46888f1ffabc8affe92
Bonekazz <juracirdantas@gmail.com>
2024-03-22 17:42:36 -0300

    fix > onUserPomodorListChanged as a member func and not a prop

--- src/controllers/userPomodor.controller.ts
+++ src/controllers/userPomodor.controller.ts
@@ -9,25 +9,31 @@
  * @param view
  */
 
+import { UserPomodor } from "../models/userPomodor.model";
 import { UserPomodorService } from "../services/userPomodor.service";
 //import { UserPomodorView } from "../views/userPomodor.view";
 
 export class UserPomodorController {
-    private _userPomodorService: UserPomodorService;
-    private onUserPomodorListChanged: Function;
+    private userPomodorService: UserPomodorService;
 
     constructor(userPomodorService: UserPomodorService) { 
-        this._userPomodorService = userPomodorService;
+        this.userPomodorService = userPomodorService;
 
+        this.init_bindings();
         
-
         // Display initial users
-        this.onUserPomodorListChanged(this._userPomodorService.userPomodorList);
+        this.onUserPomodorListChanged(this.userPomodorService.userPomodorList);
     }
 
     init_bindings() {
-        this._userPomodorService.bindUserPomodorListChanged(this.onUserPomodorListChanged);
+        this.userPomodorService.bindUserPomodorListChanged(this.onUserPomodorListChanged);
         //this.userPomodorView.bindAddUser(this.handleAddUser);
+    }
+
+    onUserPomodorListChanged(userPomodors: UserPomodor[]) {
+        //this.userPomodorView.displayUserPomodorList(userPomodors);
+        console.log("displaying users:");
+        console.log(userPomodors);
     }
 
     //handleAddUser(){}
