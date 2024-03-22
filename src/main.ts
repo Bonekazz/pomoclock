c06f18564aae3544bf6d25e571d3d1a6918af1f5
Bonekazz <juracirdantas@gmail.com>
2024-03-22 17:41:16 -0300

    feat > initializing  a userPomodor controller

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,4 @@
-console.log("oi");
+import { UserPomodorController } from "./controllers/userPomodor.controller";
+import { UserPomodorService } from "./services/userPomodor.service";
+
+const userPomodorController = new UserPomodorController(new UserPomodorService);
