4ae313e195dc0661bf42b7d844962c6db53dfb92
Bonekazz <juracirdantas@gmail.com>
2024-03-23 15:30:49 -0300

    feat > initalize lucide icons

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,15 @@ import { UserPomodorController } from "./controllers/userPomodor.controller";
 import { UserPomodorService } from "./services/userPomodor.service";
 import { UserPomodorView } from "./views/userPomodor.view";
 
+import { createIcons, Menu, Eye, Ellipsis, Plus} from "lucide";
+
+createIcons({
+    icons: {
+        Menu,
+        Eye,
+        Ellipsis,
+        Plus
+    }
+});
+
 const userPomodorController = new UserPomodorController(new UserPomodorService, new UserPomodorView);
